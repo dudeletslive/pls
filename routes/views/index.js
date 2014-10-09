17783c18515c61c7a1b3c8a0458466b1e669f35c
DudeLetsLive <dannycansignup@gmail.com>
2014-10-09 17:17:36 -0400

    Content updates

--- routes/views/index.js
+++ routes/views/index.js
@@ -8,6 +8,8 @@ exports = module.exports = function(req, res) {
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'home';
+
+	req.flash('info', 'MyLetterService is currently undergoing live maintenance as we make the transition to our new site and database. You can still access your database at <a href="http://www.mydatabaseservice.org">MyDatabaseService</a> in the mean time. During this live maintenance period you may experience brief disruptions of service. We apologize for any inconvience this may cause.');
 	
 	// Render the view
 	view.render('index');
