403b01a98d425db83ccf92739d14df084ad2a9f5
DudeLetsLive <dannycansignup@gmail.com>
2014-10-16 12:08:36 -0600

    Changed default fields

--- routes/views/test.js
+++ routes/views/test.js
@@ -1,18 +1,14 @@
 var keystone = require('keystone'),
-	Enquiry = keystone.list('Enquiry'),
-	fs = require('fs');
+	Enquiry = keystone.list('Enquiry');
 
 exports = module.exports = function(req, res) {
   
 	var view = new keystone.View(req, res),
 		locals = res.locals;
 
-		fs.writeFile('helloworld.txt', 'Hello World!', function (err) {
-			if (err) return console.log(err);
-			console.log('Hello World > helloworld.txt');
-		});
-
-		fs.readdir('./tmp');
+	if (req.method === 'POST') {
+		console.log('Posted ' + JSON.stringify(req.files));
+	}
   
 	view.render('testCSV');
   
