23921448853dd0e65c976d430800346aa88f19b3
DudeLetsLive <dannycansignup@gmail.com>
2014-11-11 11:35:47 -0500

    Mailing List endpoint

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,6 +3,7 @@ var async = require('async'),
 	auth = require('basic-auth');
  
 var Contact = keystone.list('Contact'),
+	List = keystone.list('Mailing Lists'),
 	username = 'PrayerLetterService',
 	password = 'd41d8cd98f00b204e9800998ecf8427e';
 
@@ -114,4 +115,33 @@ exports.remove = function(req, res) {
 		
 	});
 }
+
+/**
+ * Mailing List Endpoint
+ */
+
+
+/**
+ * List Mailing Lists
+ */
+exports.mailingLists = function(req, res) {
+
+	// var credentials = auth(req);
+
+	// if (!credentials || credentials.name !== 'PrayerLetterService' || credentials.pass !== password) {
+	// 	req.flash('error', 'API Auth failed');
+	// 	res.redirect('/');
+	// }
+
+	List.model.find(function(err, items) {
+		
+		if (err) return res.apiError('database error', err);
+		
+		res.apiResponse({
+			mailingLists: items
+		});
+		
+	});
+}
+
  
--- routes/index.js
+++ routes/index.js
@@ -149,11 +149,12 @@ exports = module.exports = function(app) {
 	//My Account
 	app.all('/my-account', middleware.requireUser, routes.views.session.myAccount);
 
-	// API
+	// API Routes
 	app.get('/api/v1/contacts/list', keystone.initAPI, routes.api.contacts.list);
 	app.all('/api/v1/contacts/create', keystone.initAPI, routes.api.contacts.create);
 	app.all('/api/v1/contacts/update/:id', keystone.initAPI, routes.api.contacts.update);
 	app.all('/api/v1/contacts/remove/:id', keystone.initAPI, routes.api.contacts.remove);
+	app.all('/api/v1/mailing-lists/list', keystone.initAPI, routes.api.mailingLists.list);
 
 	// Test CSV
 	app.all('/test', routes.views.test);
