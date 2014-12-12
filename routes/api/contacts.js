1252c30f4581a967ed23eb6dfe12264c9a7e7567
DudeLetsLive <dannycansignup@gmail.com>
2014-12-12 14:20:21 -0500

    List generation

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -37,7 +37,7 @@ exports.list = function(req, res) {
 exports.create = function(req, res) {
 	
 	// TODO: Add Contacts to MPDX Mailing List
-	console.log('PUT: ', req.body);
+	console.log('PUT: ', req);
 
 	// Find User by Authorization
 	User.model.findOne({'services.MPDX.accessToken': req.headers.authorization}).exec(function (err, user) {
